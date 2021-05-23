e142d7288152c520fe215fdda2fddc7b40a5d05f
nonanik112 <METEHAN_1306@hotmail.com>
2021-05-23 15:30:53 +0300

    Initial commit

---
+++ Machine_ML/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data; 
+
+namespace Machine_ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<RankingData>("./ranking.tsv", separatorChar: '\t');
+
+            var trainTestSplit = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+            var settings = new RankingExperimentSettings
+            {
+                MaxExperimentTimeInSeconds = 150,
+                OptimizingMetric = RankingMetric.Ndcg,
+            };
+
+            var experiment = context.Auto().CreateRankingExperiment(settings);
+
+            var progressHandler = new Progress<RunDetail<RankingMetrics>>(ph =>
+            {
+                if (ph.ValidationMetrics != null)
+                {
+                    Console.WriteLine($"Current trainer - {ph.TrainerName} with nDCG {ph.ValidationMetrics.NormalizedDiscountedCumulativeGains.Average()}");
+                }
+            });
+            var results = experiment.Execute(trainTestSplit.TrainSet, validationData:trainTestSplit.TrainSet, progressHandler: progressHandler);
+            var bestRun = results.BestRun;
+            var metrics = bestRun.ValidationMetrics.NormalizedDiscountedCumulativeGains;
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"Best Model {bestRun.TrainerName} with nDCG {metrics.Average()}");
+        }
+    }
+}
